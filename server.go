2b3a93dc3c9e8425a916ed807625a6607b551c54
Fisherman August <fshrmn.august@gmail.com>
2025-08-14 21:12:04 +0300

    mongodb connection

--- server.go
+++ server.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type User struct {
@@ -17,7 +20,14 @@ type User struct {
 var client *mongo.Client
 
 func main() {
-	
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer client.Disconnect(ctx)
 
 	http.HandleFunc("/users", usersHandler)
 	fmt.Println("Server is started...")
